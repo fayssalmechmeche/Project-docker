5363b2807482ab084591cd65318e3a80fad36159
Ryan Malonzo <64932650+yusa-ai@users.noreply.github.com>
2024-02-26 15:31:39 +0100

    Add comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Démarrer le serveur PostgreSQL et attendre qu'il soit prêt
+# Démarre le serveur PostgreSQL et attendre qu'il soit prêt
+# https://github.com/docker-library/postgres/issues/482#issuecomment-544977957
 pg_ctl -D /var/lib/postgresql/data -o "-c listen_addresses=*" -w start
 
 # Crée l'utilisateur s'il n'existe pas
@@ -7,5 +8,5 @@ if ! psql -tXAc "SELECT 1 FROM pg_roles WHERE rolname='$POSTGRES_USER'" | grep -
     psql -c "CREATE USER $POSTGRES_USER WITH SUPERUSER LOGIN PASSWORD '$POSTGRES_PASSWORD';"
 fi
 
-# Garde le container allumé
+# Garde le conteneur allumé
 tail -f /dev/null
