436a0437bacf632d147afe27e5d49364de326d80
Ryan Malonzo <64932650+yusa-ai@users.noreply.github.com>
2024-03-01 11:19:53 +0100

    Make Todo controller the default route

--- app/src/Controller/TodoController.php
+++ app/src/Controller/TodoController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/todo')]
+#[Route('/')]
 class TodoController extends AbstractController
 {
     #[Route('/', name: 'app_todo_index', methods: ['GET'])]
