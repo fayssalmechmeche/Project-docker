1006dbbbd95e143f4a1ec8acce4134873d16bcd3
Ryan Malonzo <64932650+yusa-ai@users.noreply.github.com>
2024-03-03 10:44:38 +0100

    Fix migration

--- app/migrations/Version20240302204939.php
+++ app/migrations/Version20240302204939.php
@@ -42,7 +42,6 @@ final class Version20240302204939 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE todo_id_seq CASCADE');
         $this->addSql('DROP TABLE todo');
         $this->addSql('DROP TABLE messenger_messages');
